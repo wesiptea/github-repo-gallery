299f1357679b8274859fe9658f8a231bc87fcae2
Anne SK <92764640+wesiptea@users.noreply.github.com>
2022-05-10 18:41:21 -0700

    Finished part 1 of github-repo-gallery

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const overview = document.querySelector(".overview");
+// overview element will inc profile info
+const username = "wesiptea";
+// my github user name
+
+const profileAPI = async function () {
+    const myInfoFetch = await fetch (`https://api.github.com/users/${username}`);
+    const data = await myInfoFetch.json();
+    displayUserData(data);
+    console.log(data);
+};
+profileAPI();
+
+const displayUserData = function (data) {
+    const div = document.createElement("div");
+    div.classList.add(".user-info");
+    div.innerHTML = `
+    <figure><img alt="user avatar" src=${data.avatar_url} /></figure>
+  <div>
+    <p><strong>Name:</strong> ${data.name}</p>
+    <p><strong>Bio:</strong> ${data.bio}</p>
+    <p><strong>Location:</strong> ${data.location}</p>
+    <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+  </div> 
+  `;
+
+  overview.append(div);
+}
